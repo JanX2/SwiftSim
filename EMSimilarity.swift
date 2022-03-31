113e77c318f2e6bd4256514420c0b5a0d6219e72
Jan Weiß <jan@geheimwerk.de>
2022-03-31 11:35:21 +0200

    Add copyright notice to header.

--- EMSimilarity.swift
+++ EMSimilarity.swift
@@ -8,6 +8,7 @@
 //  The MIT License (MIT)
 //
 //  Copyright (c) 2016 Evan Moss
+//  Copyright (c) 2022 Jan Weiß
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to deal
